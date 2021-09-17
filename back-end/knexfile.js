a5a04ad016fa0d431466aacce0c5e77afe2ccdb2
Andrew Jenkins <jenkins_aj@yahoo.com>
2021-09-17 18:39:43 +0100

    fix

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const path = require("path");
 
 const {
-  DATABASE_URL = "postgres://ehubuivj:Zk-0F6x2GT9ar_LvQkUv_nIhQ-twdb5H@kashin.db.elephantsql.com/ehubuivj",
+  DATABASE_URL = "postgresql://postgres@localhost/postgres",
   DATABASE_URL_DEVELOPMENT = "postgresql://postgres@localhost/postgres",
   DATABASE_URL_TEST = "postgresql://postgres@localhost/postgres",
   DATABASE_URL_PREVIEW = "postgresql://postgres@localhost/postgres",
