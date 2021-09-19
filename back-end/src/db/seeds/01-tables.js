1a54dd7da22109150ef24325cc66ebd3e4738710
Andrew Jenkins <jenkins_aj@yahoo.com>
2021-09-19 23:27:58 +0100

    seed

--- back-end/src/db/seeds/01-tables.js
+++ back-end/src/db/seeds/01-tables.js
@@ -2,5 +2,5 @@ const tables = require("./01-tables.json")
 exports.seed = function (knex) {
   return knex
     .raw("TRUNCATE TABLE tables RESTART IDENTITY CASCADE")
-    .then(() => ("tables").insert(tables));
+    .then(() => knex("tables").insert(tables));
 };
